d148f16eb95c7f9b48a813b31c79baa5d44f3c08
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2023-12-19 10:58:12 -0300

    Expressões aritiméticas um pouco diferente!!!

--- src/br/com/generics/estruturasequencial/ExpressoesAritmeticas.java
+++ src/br/com/generics/estruturasequencial/ExpressoesAritmeticas.java
@@ -1,2 +1,50 @@
-package br.com.generics.estruturasequencial;public class ExpressoesAritmeticas {
+package br.com.generics.estruturasequencial;
+
+import javax.swing.*;
+
+
+public class ExpressoesAritmeticas {
+
+    public static void main(String[] args) {
+        int calc1, calc2, calc3, expres;
+        float result;
+
+        expres = Integer.parseInt(JOptionPane.showInputDialog("Digite o número 1(um) para a expressão multiplicações: \n" +
+                "Digite o número 2(dois) para a expressão divisões: \n " +
+                "Digite o numero 3(três) para a expressão soma: \n" +
+                "Digite o numero 4(quatro) para a expressão subtração: "));
+
+    if(expres == 1) {
+        calc1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor a ser calculado: "));
+        calc2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor a ser calculado: "));
+        calc3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro valor a ser calculado: "));
+
+        result = calc1 * calc2 * calc3;
+        JOptionPane.showMessageDialog(null, result);
+
+    } else if(expres == 2) {
+        calc1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor a ser calculado: "));
+        calc2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor a ser calculado: "));
+        calc3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro valor a ser calculado: "));
+
+        result = calc1 / calc2 / calc3;
+        JOptionPane.showMessageDialog(null, result);
+
+    } else if(expres == 3) {
+        calc1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor a ser calculado: "));
+        calc2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor a ser calculado: "));
+        calc3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro valor a ser calculado: "));
+
+        result = calc1 - calc2 - calc3;
+        JOptionPane.showMessageDialog(null, result);
+
+    } else if(expres == 4) {
+        calc1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor a ser calculado: "));
+        calc2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor a ser calculado: "));
+        calc3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro valor a ser calculado: "));
+
+        result = calc1 + calc2 + calc3;
+        JOptionPane.showMessageDialog(null, result);
+      }
+    }
 }
