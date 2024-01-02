4e479bda28e100faa1008e5e9ee49b9a35e6b648
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 14:00:28 -0300

    Primary program with coment

--- src/br/com/generics/estruturasequencial/PrimaryProgram.java
+++ src/br/com/generics/estruturasequencial/PrimaryProgram.java
@@ -3,6 +3,7 @@ package br.com.generics.estruturasequencial;
 import javax.swing.*;
 
 public class PrimaryProgram {
+
     public static void main(String[] args) {
         System.out.println("Hello, Word!!!");
         JOptionPane.showMessageDialog(null, "Hello, Word");
