f13a660444936929b01d9bd7b37df5e9dbab62a1
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 13:57:45 -0300

    Primary program with coment

---
+++ src/br/com/generics/estruturasrepetitivas/Test.java
@@ -0,0 +1,14 @@
+package br.com.generics.estruturasrepetitivas;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int gender;
+        
+    }
+}
