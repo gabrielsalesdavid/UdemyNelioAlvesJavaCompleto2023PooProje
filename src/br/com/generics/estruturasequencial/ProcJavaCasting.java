e5b777bfa25c1ce4df192c47997f076aa2f54442
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 16:24:40 -0300

    proc em java cast

---
+++ src/br/com/generics/estruturasequencial/ProcJavaCasting.java
@@ -0,0 +1,17 @@
+package br.com.generics.estruturasequencial;
+
+public class ProcJavaCasting {
+
+    public static void main(String[] args) {
+
+        //int seria o tipo de variavel, x e y o nome da variavel
+        int x;
+        double y;
+        x = 5;
+        y = 2 * x;
+
+        //SysOut saida de dados
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
