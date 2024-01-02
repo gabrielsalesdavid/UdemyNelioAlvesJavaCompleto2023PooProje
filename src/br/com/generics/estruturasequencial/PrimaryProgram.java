c3324067ec657713055065e8a914ab1564778510
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 14:07:00 -0300

    Primary program with coment

--- src/br/com/generics/estruturasequencial/PrimaryProgram.java
+++ src/br/com/generics/estruturasequencial/PrimaryProgram.java
@@ -7,6 +7,6 @@ public class PrimaryProgram {
     public static void main(String[] args) {
         System.out.println("Hello, Word!!!");
         JOptionPane.showMessageDialog(null, "Hello, Word");
-        // SysOut/JOption.showMessage, são a saida de mensagem para users!!!
+        // SysOut/JOption.showMessage, são a saida de mensagem para users. O sys mostra na propria VM e o JOption é um PoUp com a MSG!!!
     }
 }
