a658a197cacaa0f2033d2f20e0715e35c9ff1ca8
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 16:09:55 -0300

    Concatenation of variables

---
+++ src/br/com/generics/estruturasequencial/ConcatVariavel.java
@@ -0,0 +1,18 @@
+package br.com.generics.estruturasequencial;
+
+import java.util.Locale;
+
+public class ConcatVariavel {
+
+    public static void main(String[] args) {
+
+        //double é o tipo de variavel, metros seria o nome do tipo de variavel....
+        double metros = 10.35784;
+
+        //Locale.setDefault seria a conversao de dados para a localidade USA.
+        Locale.setDefault(Locale.US);
+
+        //SysOutF e a saida de dados formatado concatenando a variavel e a MSG.
+        System.out.printf("Resultado %s", metros + " Metros.");
+    }
+}
