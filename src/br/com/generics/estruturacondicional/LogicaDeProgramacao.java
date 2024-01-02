5846d2f06483dddd4ccd90c294be7ae5b66d117b
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 15:12:28 -0300

    Adding comments

--- src/br/com/generics/estruturacondicional/LogicaDeProgramacao.java
+++ src/br/com/generics/estruturacondicional/LogicaDeProgramacao.java
@@ -6,13 +6,28 @@ public class LogicaDeProgramacao {
 
     public static void main(String[] args) {
 
+        /*
+        Scanner é um tipo de variavel que recebe a entrada que está sendo digitada pelo user.
+        Já sc é o nome da variavel.
+        (System.in) é a entrada de dados do system
+         */
         Scanner sc = new Scanner(System.in);
 
+        /*
+        String é o tipo de variavel que recebe todos os tipos de caracters.
+         */
         String name, registra, univer, disc1, disc2, disc3, disc4, disc5, disc6, pergunt;
+        /*
+        double é o tipo de variavel com pono flutuante.
+         */
         double noteDisc1, noteDisc2, noteDisc3, noteDisc4, noteDisc5, noteDisc6,
                 noteBi1, noteBi2, noteSe1, noteSe2, result;
+        /*
+        char é o tipo de variavel que recebe apenas um ou dois caracter, como se fosse o UF para estado.
+         */
         char approved;
 
+        //SysOut é meio que uma class/method para instanciar uma mensagem ao user.
         System.out.println("Digite o seu nome completo: ");
         name = sc.nextLine();
         System.out.println("Digite a sua matricula da universidade: ");
@@ -20,6 +35,13 @@ public class LogicaDeProgramacao {
         System.out.println("Digite o nome da instituicao de ensino: ");
         univer = sc.nextLine();
 
+        /*
+        if seria uma comparacao entre um objeto/variavel.
+        Sendo que tendo a resposta adequada a logica a resposta é emitida.
+        euqals, é um objeto especifico que compara os dados inseridos com o desejados.
+        else if, seria uma outra comparacao caso a anterior nao seja o esperado.
+        sc.close, finaliza o servico da variavel.
+         */
         if (univer.equals("Cruzeiro do Sul") || univer.equals("cruzeiro do sul")) {
             System.out.println("Digite o bimestre: ");
             pergunt = sc.nextLine();
--- src/br/com/generics/estruturasequencial/ExemploOperdMod.java
+++ src/br/com/generics/estruturasequencial/ExemploOperdMod.java
@@ -3,16 +3,19 @@ package br.com.generics.estruturasequencial;
 public class ExemploOperdMod {
 
     public static void main(String[] args) {
+        //int/double é o tipo de variavel. number1/2 e result1/2 é o nome da variavel instanciada.
         int number1, number2;
         double result1, result2;
 
+        //nome do tipe de variavel recendo o valor.
         number1 = 14;
         number2 = 19;
 
+        //A variavel recebendo outra com os calc.
         result1 = number1 % 3;
         result2 = number2 % 5;
 
-        //SysOut
+        //SysOut.
         System.out.println(result1);
         System.out.println(result2);
 
