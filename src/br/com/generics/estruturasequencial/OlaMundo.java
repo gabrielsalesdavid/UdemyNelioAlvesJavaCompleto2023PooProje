bd767351fba39aa0c5673114984981cbd723fced
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 14:13:03 -0300

    Fixing the public class

--- src/br/com/generics/estruturasequencial/OlaMundo.java
+++ src/br/com/generics/estruturasequencial/OlaMundo.java
@@ -2,13 +2,14 @@ package br.com.generics.estruturasequencial;
 
 import javax.swing.*;
 
-public class PrimaryProgram {
+public class OlaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hello, Word!!!");
         JOptionPane.showMessageDialog(null, "Hello, Word");
-        /* SysOut/JOption.showMessage, são a saida de mensagem para users.
-         O sys mostra na propria VM e o JOption é um PoUp com a MSG!!!
+        /*
+        SysOut/JOption.showMessage, são a saida de mensagem para users.
+        O sys mostra na propria VM e o JOption é um PoUp com a MSG!!!
          */
     }
 }
