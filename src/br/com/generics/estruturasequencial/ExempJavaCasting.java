f014551a59d8d223b985983656314add142b8c63
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 16:29:18 -0300

    exemp de dados em java cast

---
+++ src/br/com/generics/estruturasequencial/ExempJavaCasting.java
@@ -0,0 +1,18 @@
+package br.com.generics.estruturasequencial;
+
+public class ExempJavaCasting {
+
+    public static void main(String[] args) {
+
+        //double e o tipo de variavel.
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);
+    }
+}
