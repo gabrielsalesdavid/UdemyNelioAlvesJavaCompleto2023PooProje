8ad3ec54373d53568d5f66b5d5757f68b7e7da3c
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 15:17:27 -0300

    Adding comments

--- src/br/com/generics/estruturasequencial/DeclaracaoVariaveis.java
+++ src/br/com/generics/estruturasequencial/DeclaracaoVariaveis.java
@@ -6,10 +6,12 @@ public class DeclaracaoVariaveis {
 
     public static void main(String[] args) {
 
+        //String, double, int, char, float e boolean é o tipo de vaiavel.
         String address, name;
         double height;
         int gender;
 
+        //input é a entrada de dados que certa varival recebra pelo user.
         name = JOptionPane.showInputDialog("Digite o seu nome completo: ");
         address = JOptionPane.showInputDialog(" Digite o seu endereço completo: ");
 
@@ -17,10 +19,12 @@ public class DeclaracaoVariaveis {
 
         gender = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade: "));
 
+        //if seria a comparacao de variaveis/objeto. Dado o experado a resposta sera a correspondente.
         if (name.equals("Gabriel Sales David") || name.equals("gabriel sales david")) {
             JOptionPane.showMessageDialog(null, "");
         }
 
+        //JOptionPane é uma class/method que seria o input para user
         JOptionPane.showMessageDialog(null, "Seu nome é: " + name + "Endereço: " + address +
                 ", sua altura é: " + height + ", sua idade: " + gender);
     }
