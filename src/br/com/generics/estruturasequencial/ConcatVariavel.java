72dbd39955b3f6ab73a829c2b9e01c2393153f40
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-02 16:12:39 -0300

    Adding comments

--- src/br/com/generics/estruturasequencial/ConcatVariavel.java
+++ src/br/com/generics/estruturasequencial/ConcatVariavel.java
@@ -12,7 +12,10 @@ public class ConcatVariavel {
         //Locale.setDefault seria a conversao de dados para a localidade USA.
         Locale.setDefault(Locale.US);
 
-        //SysOutF e a saida de dados formatado concatenando a variavel e a MSG.
+        /*
+        SysOutF e a saida de dados formatado concatenando a variavel e a MSG.
+        %s seria para txt, f seria ponto flutuante, d inteiro, n quebra de linha.
+         */
         System.out.printf("Resultado %s", metros + " Metros.");
     }
 }
