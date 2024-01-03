ae4091b251724f953c3e95053d452f553ea55e2d
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-03 14:06:38 -0300

    While com atribuicao acumulativa!

--- src/br/com/generics/estruturasrepetitivas/Enquant.java
+++ src/br/com/generics/estruturasrepetitivas/Enquant.java
@@ -2,7 +2,6 @@ package br.com.generics.estruturasrepetitivas;
 
 import java.util.Scanner;
 public class Enquant {
-
     public static void main(String[] args) {
 
         //Scanner e um method/class que escaneia dados que sera digitados pelo user.
@@ -13,11 +12,15 @@ public class Enquant {
         sc.nextIntescaneia dados do tipo inteiro.
         while e uma estrutura de repeticao que nao se sabe previamente quando sera preciso para repetir uma condicao.
         o nome da variavel sendo comparada se é diferente com o valor declarado.
+        nome da variavel recebendo ela mesma concatenado com outro nome de variavel.
+        += significa atribuicao acumulativa.
         nome da variavel recebendo novamente o scann para comparar o que foi digitado, caso seja o esperado ele para.
          */
-        int x;
+        int x, soma;
+        soma = 0;
         x = sc.nextInt();
         while (x != 0) {
+            soma += x;
             x = sc.nextInt();
         }
         sc.close();
