791d349ea0ca28a3d568a9f2ad4a968e845a8ee9
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-03 10:40:53 -0300

    expressoes logicas

--- src/br/com/generics/estruturacondicional/ExempExpressLogic.java
+++ src/br/com/generics/estruturacondicional/ExempExpressLogic.java
@@ -17,12 +17,31 @@ public class ExempExpressLogic {
         !(x == y && b > a || c < z). o sinal de esclamacao(!) significa a negacao de algo na logica de programacao.
         ja o sinal de dupla igualdade(==), significa que um objeto/variaveis é igual a outro.
         Os sinais de "E" comerciais(&&), é uma logica de progamacao, que se resulta apenas se duas condicoes estao verdadeiras
-        Ja o sinais de pipeline duplo(||), significa que apena uma condicao seja verdadeira
+        Ja o sinais de pipeline duplo(||), significa que apena uma condicao seja verdadeira.
         Os sinais maior e menor(<>), faz com que se uma variavel é menor ou maior do que a outra.
-        Ja os sinais menor/maior igual a(<=/>=), significa que tudo que for menor/maior igual a uma variavel
+        Ja os sinais menor/maior igual a(<=/>=), significa que tudo que for menor/maior igual a uma variavel.
+        o sinasl de esclamacao seguida de igualdade(!=), significa que a variavel/condicoes e diferente.
          */
         if (!(x <= 20 && x == 10)) { //essa logica sem o sinal de esclamacao(!) equivale a falso, sendo assim com o sinal de esclamacao a logica fica verdadeiro
-            System.out.println("É verdadeiro!!");
+            System.out.println("É verdadeiro!");
+        }
+        if (x <= 20 && x == 10) {
+            System.out.println("Falso!");
+        }
+        if (x > 0 && x != 3) {
+            System.out.println("Verdadeiro!!");
+        }
+        if (x <= 20 && x == 10 && x != 3) {
+            System.out.println("Falso!");
+        }
+        if (x == 10 || x <= 20) {
+            System.out.println("Verdadeiro!!!");
+        }
+        if (x > 0 || x != 3) {
+            System.out.println("Verdadeiro!!!!");
+        }
+        if (x <= 20 || x == 10 || x != 3) {
+            System.out.println("Verdadeiro!!!!!!");
         }
     }
 }
