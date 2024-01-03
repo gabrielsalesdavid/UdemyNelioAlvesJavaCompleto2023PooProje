6fb8ca38304302f2f2d145bb3e6b8f6eab6807a7
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-03 10:29:33 -0300

    expressoes logicas

---
+++ src/br/com/generics/estruturacondicional/ExempExpressLogic.java
@@ -0,0 +1,28 @@
+package br.com.generics.estruturacondicional;
+
+public class ExempExpressLogic {
+
+    public static void main(String[] args) {
+
+        /*
+        int é o tipo de variavel. ja o X seria o nome da variavel.
+        X com o sinal de igualdade, significa que ele esta recebendo um valor.
+         */
+        int x;
+        x = 5;
+
+        /*
+        if significa que se uma coisa com a lgica ou a comparacao de dados declarada dentro dela. Caso seja o esperado
+        o if joga uma mensagem, do contrario ela pula para o else if ou apenas no else que seria a finalizacao do src.
+        !(x == y && b > a || c < z). o sinal de esclamacao(!) significa a negacao de algo na logica de programacao.
+        ja o sinal de dupla igualdade(==), significa que um objeto/variaveis é igual a outro.
+        Os sinais de "E" comerciais(&&), é uma logica de progamacao, que se resulta apenas se duas condicoes estao verdadeiras
+        Ja o sinais de pipeline duplo(||), significa que apena uma condicao seja verdadeira
+        Os sinais maior e menor(<>), faz com que se uma variavel é menor ou maior do que a outra.
+        Ja os sinais menor/maior igual a(<=/>=), significa que tudo que for menor/maior igual a uma variavel
+         */
+        if (!(x <= 20 && x == 10)) { //essa logica sem o sinal de esclamacao(!) equivale a falso, sendo assim com o sinal de esclamacao a logica fica verdadeiro
+            System.out.println("É verdadeiro!!");
+        }
+    }
+}
