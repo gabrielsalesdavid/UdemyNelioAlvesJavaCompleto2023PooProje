5f96b3753e99984009f71848e8d06cf8e3803fd4
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-04 08:39:38 -0300

    Add coment atribut

--- src/br/com/generics/poo/Triangle.java
+++ src/br/com/generics/poo/Triangle.java
@@ -1,7 +1,9 @@
 package br.com.generics.poo;
 
 public class Triangle {
-
+    /*
+    os publics doubles e do tipo de atributo declarada o nome da variavel que recebera e transmitira dados a outro atributos.
+     */
     public double a;
     public double b;
     public double c;
