3f69e14092f6c890f50dbf44d89a6155711ee400
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-05 14:09:55 -0300

    class encapsulamentos

---
+++ src/br/com/generics/encapsulamento/Enca.java
@@ -0,0 +1,60 @@
+package br.com.generics.encapsulamento;
+
+import br.com.generics.sobrecarga.SobCarg;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Enca {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Encapsu enc = new Encapsu();
+
+        /*
+        Foi realizado a declaracao das variaveis temp na condicao.
+        sendo que se fosse depois dos atributos/method Locale e Scanner a condicao nao reconheceria,
+        assim foi inserido as variaveis temp depois de cada condicao.
+         */
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Price: ");
+        double price = sc.nextDouble();
+
+        Encapsu ec = new Encapsu(name, price);
+
+        ec.setName("Nome que deseja ser alterado!");
+
+        System.out.println(name + ", " + price);
+        System.out.println(ec);
+
+        /*
+        A class foi declarada por aqui. pois se fosse antes das variaveis e depois do method a condicao
+        nao reconheceria. e uma class construtora. assim elemina alguns codigos feitos e deixa tudo centrado nele.
+        nomes das variaveis parametrizada no SysOut/Inicializada ela refere a class/atributo Encapsu.
+        sendo assim, essa class principal seria o coletor de daods e repasa a outra class que realiza o armezanamento
+        dos dados inseridos. como se fosse um BD local!
+        Na sobre carga, foi retirado a variavel quantity do method. sendo que o codigo diminuiu um pouco.
+        */
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        ec.addProducts(quantity);
+
+        System.out.println(name + ", " + price + ", " + quantity);
+        System.out.println(ec);
+
+        System.out.println("Enter the number of products to be remove from stock: ");
+        quantity = sc.nextInt();
+        ec.removeProducts(quantity);
+
+        System.out.println(name + ", " + price + ", " + quantity);
+        System.out.println(ec);
+
+        sc.close();
+    }
+}
---
+++ src/br/com/generics/encapsulamento/Encapsu.java
@@ -0,0 +1,72 @@
+package br.com.generics.encapsulamento;
+
+public class Encapsu {
+
+    /*
+    Encapsulamento seria a forma de passar um atributo para private.
+    Sendo assim o atributo nao e acessivel para qualquer class.
+     */
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Encapsu() {
+    }
+
+    /*
+    public Encapsu e um method construtor publico. Sendo instanciado/parametrizado as variaveis String, double e int.
+    this seria uma referencia entre objetos/atributos.
+    sendo que não usado o this o nome da varaivel se referenciar na instanciacao/parametro do method local.
+    por isso se usa o this para referenciar um objeto/atributo.
+    Usasse um method public com a variavel setObject/Variavel. Serve para coletar os dados e altera.
+    getObject/Variavel e como joga o dado alterado! o get/set e usado para ser usado para entrada e saida de dados.
+    por regra de negocio não se usa o set para somente a saida de dados e nao pela entrada!
+    Passando na class principal ele com o valor para ser alterado.
+     */
+    public Encapsu(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double totalValueInSotck() {
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $"
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, total: $ "
+                + String.format("%.2f", totalValueInSotck());
+    }
+}
