42bb987a15ca8ab17a7f13e5a8c4a7e2f1c59c0d
gabrielsalesdavid <gabrielsalesdavid@hotmail.com>
2024-01-08 12:46:35 -0300

    corrig the comment

--- src/br/com/generics/listas/BoxingUnBox.java
+++ src/br/com/generics/listas/BoxingUnBox.java
@@ -7,7 +7,7 @@ public class BoxingUnBox {
         /*
         int e do tipo inteiro que declarado o x/y como representado a variavel do tipo inteiro primitivo.
         nome da variavel recebe(=) o valor isntanciado;
-        object e uma variavel da class boject. obj recebe(=) x que recebeu o valor 20. isto seria boxing.
+        object e uma variavel da class object. obj recebe(=) x que recebeu o valor 20. isto seria boxing.
         ja o unboxing seria nome da variavel, y recebe um casting do tipo inteiro obj,
         faz com que o nome da class/varaivel obj seja convertido a variavel do tipo inteiro.
          */
