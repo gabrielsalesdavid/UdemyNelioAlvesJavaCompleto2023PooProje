de84d30fde43c76e2185496e8ffdb8e63ab70b15
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-01-15 16:13:44 -0300

    CalcDate add coment

--- src/br/com/generics/topicosavancadosemjava/CalcDate.java
+++ src/br/com/generics/topicosavancadosemjava/CalcDate.java
@@ -8,6 +8,12 @@ public class CalcDate {
 
     public static void main(String[] args) {
 
+        /*
+        Scanner e uma class/method do tipo primitivo que scanneia dados que sera digitados.
+        int e o tipo de variavel do tipo primitivo.
+        SysOut e a saida de dados para user.
+        dtNsc e nome da variavel do tipo inteiro que recebe o nome da variavel/method sc que scanneia os dados.
+         */
         Scanner sc = new Scanner(System.in);
 
         int dtNsc;
@@ -15,6 +21,18 @@ public class CalcDate {
         System.out.println("Digite sua data de nascimento: ");
         dtNsc = sc.nextInt();
 
+        /*
+        LocalDate e uma class/method ISO8601 que mostra a data local do sistema.
+        LocalDate.now pega as informacoes de data do momento em que o comando esta sendo executado.
+        LocalDate yeaer recebe o nome da variavel/method .minusYear(nome da variavel)
+        que recebeu os dados que user digiou.
+        .minusYear(nome da variavel) passa como o parametro 0 (dtNsc) e pega o ano - o dados que foi digitado pelo user.
+        SysOut e a saida de dados que inclementa os nomes das variaveis e os comentarios.
+        year.format(DateTimeFormatter.ofPattern("a formatacao que deseja inserir"). o .format e um method static que
+        formata o obj no formato que deseja. passando como parametro DateTimeFormatter.ofPattern("").
+        DateTimeFormtter e um method/obj que formata sem a necessidade de declaracao de variaveis.
+        .ofPattern("") e um method static que passa para o parametro da formatacao("").
+         */
         LocalDate dtHj = LocalDate.now();
 
         LocalDate year = dtHj.minusYears(dtNsc);
