7cd03681dbe0e318af0fb6f918bbaf545eb87553
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-01-15 15:57:25 -0300

    CalcDate add SysOut

--- src/br/com/generics/topicosavancadosemjava/CalcDate.java
+++ src/br/com/generics/topicosavancadosemjava/CalcDate.java
@@ -12,12 +12,18 @@ public class CalcDate {
 
         int dtNsc;
 
+        System.out.println("Digite sua data de nascimento: ");
         dtNsc = sc.nextInt();
 
         LocalDate dtHj = LocalDate.now();
 
         LocalDate year = dtHj.minusYears(dtNsc);
 
-        System.out.println(year.format(DateTimeFormatter.ofPattern("yy")));
+        System.out.println("A sua idade e: "
+                + year.format(DateTimeFormatter.ofPattern("yy"))
+                + " \nVoce nasceu no ano de : "
+                + dtNsc
+                + "\nEstamos no ano de: "
+                + dtHj.format(DateTimeFormatter.ofPattern("yyyy")));
     }
 }
